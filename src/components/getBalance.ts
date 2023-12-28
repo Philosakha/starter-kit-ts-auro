903c3cb08e12fc8eb9de11e6729c6cc133704b1d
Saviour1001 <harshghodkar@gmail.com>
2023-12-28 23:01:38 +0530

    adding new components

---
+++ src/components/getBalance.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const getAccountBalance = async (accounts: string) => {
+  const apiUrl = `https://api.minaexplorer.com/accounts/${accounts}`;
+  const response = await fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json", // Add any headers you need
+    },
+  });
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    console.log("Error");
+  }
+};
---
+++ src/components/wallets.tsx
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from "react";
+import WalletExist from "./WalletExist";
+import { getAccountBalance } from "./getBalance";
